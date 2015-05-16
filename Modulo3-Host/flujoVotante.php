5fa0c7273b8ee3240edb17dbc0a1e431bd3431a6
Haza <mojica.hazael@gmail.com>
2015-05-16 18:54:09 -0500

    Comenzando con el Host
    host...

---
+++ Modulo3-Host/flujoVotante.php
@@ -0,0 +1,46 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<title>Flujo de Votantes - Módulo 3</title>
+		<link relQuery Mobile Bootstrap ="stylesheet" href="themes/Bootstrap.css">
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.0/jquery.mobile.structure-1.4.0.min.css" />
+		<link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
+		<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.4.0/jquery.mobile-1.4.0.min.js"></script>
+	</head>
+	<body>
+		<div data-role="page" data-theme="a">
+			<div data-role="header" data-position="inline">
+				<h1>Flujo de Votantes - Módulo 3</h1>
+				<div data-role="navbar">
+					<ul>
+						<li><a href="index.html" data-icon="home" class="ui-btn-active">Datos Iniciales</a></li>
+						<li><a href="buttons.html" data-icon="star">Buttons</a></li>
+					</ul>
+				</div>
+			</div>
+			<div data-role="content" data-theme="a">
+
+				<a href="https://github.com/commadelimited/jQuery-Mobile-Bootstrap-Theme" data-role="button" data-icon="star">Get the code</a>
+
+				<h2>Overview</h2>
+
+				<p>
+					The jQuery Mobile Bootstrap theme is meant to duplicate the appearance of <a href="http://twbs.github.io/bootstrap/">Bootstrap</a> within the context of a jQuery Mobile application. Currently only the colors are mimiced, but eventually some of the components might also work their way into the theme.
+				</p>
+
+			</div>
+		</div>
+	</body>
+</html>
